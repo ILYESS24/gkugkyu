6eb7fde25eb094cab2cfcc55bfd84b41960c08c5
ILYESS24 <ilyessbenina3@gmail.com>
2025-11-19 21:51:18 +0100

    Fix: Replace @n8n/di import with @workflow-automation/di in config decorators

--- Downloads/n8n-master/n8n-master/packages/@workflow-automation/config/src/decorators.ts
+++ Downloads/n8n-master/n8n-master/packages/@workflow-automation/config/src/decorators.ts
@@ -1,5 +1,5 @@
 import 'reflect-metadata';
-import { Container, Service } from '@n8n/di';
+import { Container, Service } from '@workflow-automation/di';
 import { readFileSync } from 'fs';
 import { z } from 'zod';
 
